5e6fef45787bd3873a44bb941a64baf76c0cb4f2
Nabeela Abidi <55115500+nabeelaabidi@users.noreply.github.com>
2020-12-27 17:39:34 -0800

    Populated Genres table

---
+++ Gighub/Data/Migrations/20201228012226_PopulateGenresTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gighub.Data.Migrations
+{
+    public partial class PopulateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+            table: "Genres",
+            columns: new[] { "Id", "Name" },
+            values: new object[] { "1", "Jazz" });
+
+            migrationBuilder.InsertData(
+            table: "Genres",
+            columns: new[] { "Id", "Name" },
+            values: new object[] { "2", "Rock" });
+
+            migrationBuilder.InsertData(
+            table: "Genres",
+            columns: new[] { "Id", "Name" },
+            values: new object[] { "3", "Pop" });
+
+            migrationBuilder.InsertData(
+            table: "Genres",
+            columns: new[] { "Id", "Name" },
+            values: new object[] { "4", "Country" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres", true);
+        }
+    }
+}
